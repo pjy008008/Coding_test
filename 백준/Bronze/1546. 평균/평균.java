496e4b21c6488dbcf79af1e6665f6593b738b442
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-10 11:21:24 +0900

    [Bronze I] Title: 평균, Time: 220 ms, Memory: 19044 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int max=0;
+        double total=0;
+        double average;
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            if (x > max) {
+                max=x;
+            }
+            arr[i]=x;
+        }
+        for (int i = 0; i < n; i++) {
+            total += ((double)arr[i]/max*100);
+        }
+        average = total/n;
+        System.out.println(average);
+    }
+}
