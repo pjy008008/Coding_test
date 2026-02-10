bdbe7ed8bb5d9e3fb1f06f2faf326ebce8ac4c4b
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-10 17:30:33 +0900

    [D3] Title: N-Queen, Time: 80 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,69 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Solution {
-	private static boolean[][] impossible;
-	private static int cnt;
+	private static boolean[] c;
+	private static boolean[] dia1;
+	private static boolean[] dia2;
 	private static int n;
-	private static int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 };
-	private static int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
+	private static int cnt;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= t; i++) {
 			n = Integer.parseInt(br.readLine());
+			c = new boolean[n];
+			dia1 = new boolean[2 * n];
+			dia2 = new boolean[2 * n];
 			cnt = 0;
-			impossible = new boolean[n][n];
-			for (int j = 0; j < n; j++) {
-				dfs(0, j, 1);
-			}
+			solve(0);
 			sb.append("#").append(i).append(" ").append(cnt).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static void dfs(int x, int y, int depth) {
-		if (depth == n) {
+	private static void solve(int row) {
+		if (row == n) {
 			cnt++;
 			return;
 		}
-		List<int[]> changed = new ArrayList<>();
-		setPossible(x, y, changed);
-
-		for (int i = x + 1; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (!impossible[i][j]) {
-					dfs(i, j, depth + 1);
-				}
-			}
-		}
-		for (int[] pos : changed) {
-			impossible[pos[0]][pos[1]] = false;
-		}
-	}
-
-	private static void setPossible(int x, int y, List<int[]> changed) {
-		if (!impossible[x][y]) {
-			impossible[x][y] = true;
-			changed.add(new int[] { x, y });
-		}
-		for (int j = 1; j < n; j++) {
-			for (int i = 0; i < 8; i++) {
-				int nx = x + j * dx[i];
-				int ny = y + j * dy[i];
-				if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-					if (!impossible[nx][ny]) {
-						impossible[nx][ny] = true;
-						changed.add(new int[] { nx, ny });
-					}
-				}
+		for (int col = 0; col < n; col++) {
+			if (!c[col] && !dia1[row + col] && !dia2[row - col + n]) {
+				c[col] = dia1[row + col] = dia2[row - col + n] = true;
+				solve(row + 1);
+				c[col] = dia1[row + col] = dia2[row - col + n] = false;
 			}
 		}
 	}
