d8da8dcf630a799c8fa36b102086ad5ad97281fe
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-15 14:54:16 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15756 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n;
+    private static int m;
+    private static int[] numbers;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[m];
+        comb(0, 1);
+        System.out.println(sb);
+    }
+
+    private static void comb(int cnt, int start) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            numbers[cnt] = i;
+            comb(cnt + 1, i);
+        }
+    }
+}
