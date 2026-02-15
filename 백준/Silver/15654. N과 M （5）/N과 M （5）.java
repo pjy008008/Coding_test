27a88ed0e203f5259b2b574b934add27702858ff
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-15 19:01:43 +0900

    [Silver III] Title: N과 M (5), Time: 276 ms, Memory: 28116 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n;
+    private static int m;
+    private static int[] arr;
+    private static int[] numbers;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        numbers = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        perm(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void perm(int cnt, int flag) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((flag & (1 << i)) != 0) {
+                continue;
+            }
+            numbers[cnt] = arr[i];
+            perm(cnt + 1, flag | 1 << i);
+        }
+    }
+}
