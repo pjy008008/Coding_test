964f16b33329ede885d9cf99db02c19fee18b7e1
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-15 19:37:00 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,11 +23,11 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        perm(0, 0);
+        comb(0, 0);
         System.out.println(sb);
     }
 
-    private static void perm(int cnt, int start)  {
+    private static void comb(int cnt, int start)  {
         if (cnt == m) {
             for (int i = 0; i < m; i++) {
                 sb.append(numbers[i]).append(" ");
@@ -38,7 +37,7 @@ public class Main {
         }
         for (int i = start; i < n; i++) {
             numbers[cnt] = arr[i];
-            perm(cnt + 1, i + 1);
+            comb(cnt + 1, i + 1);
         }
     }
 }
