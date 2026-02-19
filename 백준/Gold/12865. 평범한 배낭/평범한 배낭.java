9abad29858195cf1d47f098568d77304846b06e5
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-02-19 15:41:27 +0900

    [Gold V] Title: 평범한 배낭, Time: 120 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,40 +9,18 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[n + 1][m + 1];
-		List<Thing> list = new ArrayList<>();
+		int[] dp = new int[m + 1];
+		int[][] item = new int[n][2];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			list.add(new Thing(w, v));
+			item[i][0] = Integer.parseInt(st.nextToken());
+			item[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Collections.sort(list);
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j <= m; j++) {
-				Thing t = list.get(i - 1);
-				if (j < t.weight) {
-					dp[i][j] = dp[i - 1][j];
-				} else {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - t.weight] + t.value);
-				}
+		for (int i = 0; i < n; i++) {
+			for (int j = m; j >= item[i][0]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - item[i][0]] + item[i][1]);
 			}
 		}
-		System.out.println(dp[n][m]);
-	}
-
-	static class Thing implements Comparable<Thing> {
-		int weight;
-		int value;
-
-		public Thing(int weight, int value) {
-			this.weight = weight;
-			this.value = value;
-		}
-
-		@Override
-		public int compareTo(Thing o) {
-			return this.weight - o.weight;
-		}
+		System.out.println(dp[m]);
 	}
 }
