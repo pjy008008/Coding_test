b5a9f18859193119825466e3d779e28635a9d967
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-03-01 20:07:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 420 ms, Memory: 51620 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        // init graph
+        ArrayList<Node>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // init dist
+        int[] dist = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[s].add(new Node(e, w));
+        }
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (cur.w > dist[cur.v]) {
+                continue;
+            }
+            for (Node next : graph[cur.v]) {
+                if (dist[next.v] > dist[cur.v] + next.w) {
+                    dist[next.v] = dist[cur.v] + next.w;
+                    pq.offer(new Node(next.v, dist[cur.v] + next.w));
+                }
+            }
+        }
+        System.out.println(dist[end]);
+    }
+
+    static class Node implements Comparable<Node>{
+        int v;
+        int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.w - o.w;
+        }
+    }
+}
