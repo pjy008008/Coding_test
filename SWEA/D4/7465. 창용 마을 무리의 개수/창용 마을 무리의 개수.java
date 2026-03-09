cd627c783db6ffe485c0e43a0b133b1ac41c13e4
Junyou Park <69574758+pjy008008@users.noreply.github.com>
2026-03-09 13:52:33 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 104 ms, Memory: 28,544 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static int[] parent;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			parent = new int[n + 1];
+			for (int j = 1; j <= n; j++) {
+				parent[j] = j;
+			}
+			for (int j = 0; j < m; j++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				union(a, b);
+			}
+			Set<Integer> set = new HashSet<>();
+			for (int j = 1; j <= n; j++) {
+				int p = find(j);
+				if(!set.contains(p)) {
+					set.add(p);
+				}
+			}
+			sb.append("#").append(i).append(" ").append(set.size()).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int find(int a) {
+		if (parent[a] == a) {
+			return a;
+		}
+		return parent[a] = find(parent[a]);
+	}
+
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b) {
+			parent[b] = a;
+		}
+	}
+}
