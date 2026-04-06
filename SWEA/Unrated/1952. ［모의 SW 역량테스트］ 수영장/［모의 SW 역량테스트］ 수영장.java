9e4da5d18b1c915625dcf153a3c6ed83462da893
Junyou Park <pjy008008@gmail.com>
2026-04-06 17:27:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 91 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,50 +1,61 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-    private static int[] tickets;
-    private static int[] plans;
-    private static int min;
+	private static int[] price;
+	private static int[] plan;
+	private static int[] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= t; i++) {
-            tickets = new int[4];
-            plans = new int[12];
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                tickets[j] = Integer.parseInt(st.nextToken());
-            }
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 12; j++) {
-                plans[j] = Integer.parseInt(st.nextToken());
-            }
-            min = tickets[3];
-            dfs(0, 0);
-            sb.append("#").append(i).append(" ").append(min).append("\n");
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= t; i++) {
+			// initialize price
+			price = new int[4];
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 4; j++) {
+				price[j] = Integer.parseInt(st.nextToken());
+			}
 
-    private static void dfs(int idx, int cost) {
-        if (idx >= 12) {
-            min = Math.min(min, cost);
-            return;
-        }
-        if (cost >= min) {
-            return;
-        }
+			// initialize plan
+			st = new StringTokenizer(br.readLine());
+			plan = new int[12];
+			for (int j = 0; j < 12; j++) {
+				plan[j] = Integer.parseInt(st.nextToken());
+			}
 
-        int days = plans[idx];
-        if (plans[idx] == 0) {
-            dfs(idx + 1, cost);
-        } else {
-            dfs(idx + 1, cost + tickets[0] * days);
-            dfs(idx + 1, cost + tickets[1]);
-            dfs(idx + 3, cost + tickets[2]);
-        }
-    }
+			// initialize dp
+			dp = new int[12];
+			Arrays.fill(dp, -1);
+			
+			int res = solve(0);
+			res = Math.min(res, price[3]);
+			sb.append("#").append(i).append(" ").append(res).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int solve(int idx) {
+		if (idx >= 12) {
+			return 0;
+		}
+		if (dp[idx] != -1) {
+			return dp[idx];
+		}
+		int day = plan[idx];
+
+		if (day == 0) {
+			dp[idx] = solve(idx + 1);
+		} else {
+			dp[idx] = Math.min(solve(idx + 1) + price[0] * day, solve(idx + 1) + price[1]);
+			dp[idx] = Math.min(dp[idx], solve(idx + 3) + price[2]);
+		}
+
+		return dp[idx];
+	}
 }
