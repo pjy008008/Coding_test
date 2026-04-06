4bb243988ee6eb5c68913a7d3b5375d0582dd97b
Junyou Park <pjy008008@gmail.com>
2026-04-06 15:44:04 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14360 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -10,9 +9,10 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		String s = br.readLine();
 		long sum = 0;
+		long r = 1;
 		for (int i = 0; i < n; i++) {
-			sum += (s.charAt(i) - 'a' + 1) * Math.pow(31, i);
-			sum %= 1234567891;
+			sum = (sum + (s.charAt(i) - 'a' + 1) * r) % 1234567891;
+			r = (r * 31) % 1234567891;
 		}
 		System.out.println(sum);
 	}
