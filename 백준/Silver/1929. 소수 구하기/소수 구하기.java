8b001a6d2b0d6b6ed882647e008fd9111a62909a
Junyou Park <pjy008008@gmail.com>
2026-04-07 09:07:19 +0900

    [Silver III] Title: 소수 구하기, Time: 232 ms, Memory: 23472 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n + 1];
+		for (int i = 2; i <= n; i++) {
+			arr[i] = i;
+		}
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (arr[i] == 0) {
+				continue;
+			}
+			for (int j = i + i; j <= n; j = j + i) {
+				arr[j] = 0;
+			}
+		}
+		for (int i = m; i <= n; i++) {
+			if (arr[i] != 0) {
+				sb.append(arr[i]).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
